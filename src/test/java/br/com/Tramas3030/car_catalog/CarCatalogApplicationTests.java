acda72293fd73884090925885e26928b6df24d36
Matheus Raphael <matheusraphael0902@gmail.com>
2024-08-20 14:03:30 -0300

    feat: initial project structure

---
+++ src/test/java/br/com/Tramas3030/car_catalog/CarCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.Tramas3030.car_catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
