ad2fe8892bdd04f5ba67fee755b635f663c08311
Matheus Raphael <matheusraphael0902@gmail.com>
2024-08-21 15:22:12 -0300

    feat: CreateClientUseCase

---
+++ src/main/java/br/com/Tramas3030/car_catalog/modules/client/useCase/CreateClientUseCase.java
@@ -0,0 +1,24 @@
+package br.com.Tramas3030.car_catalog.modules.client.useCase;
+
+import br.com.Tramas3030.car_catalog.exceptions.UserFounderException;
+import br.com.Tramas3030.car_catalog.modules.client.entities.ClientEntity;
+import br.com.Tramas3030.car_catalog.modules.client.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateClientUseCase {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    public ClientEntity execute(ClientEntity clientEntity) {
+        this.clientRepository.findByUsernameOrEmail(clientEntity.getUsername(), clientEntity.getEmail())
+                .ifPresent(user -> {
+                    throw new UserFounderException();
+                });
+
+        return this.clientRepository.save(clientEntity);
+    }
+
+}
